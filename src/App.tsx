bfbccb55f4509562f01010726e52bd89195ba599
tatsu <k.t.19963469@gmail.com>
2022-12-17 19:51:15 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Task } from './components/Types'
+import TaskInput from './components/TaskInput'
+import TaskList from './components/TaskList'
+
+const initialState: Task[] = [
+  // {
+  //     id: 2,
+  //     title: '次にやるやつ',
+  //     done: false
+  // },{
+  //     id: 1,
+  //     title: 'はじめにやるやつ',
+  //     done: true
+  // }
+]
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState(initialState)
+  return (
+      <div>
+          <TaskInput setTasks={setTasks} tasks={tasks} />
+          <TaskList setTasks={setTasks} tasks={tasks} />
+      </div>
+  )
+
+}
+
+export default App
